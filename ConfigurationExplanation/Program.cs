531077af0b2631c47a1ae9c2f4a1d42062656191
Mahmoud-Mshrf <104544236+Mahmoud-Mshrf@users.noreply.github.com>
2026-07-11 03:36:08 +0300

    Add initial build artifacts and configuration files
    - Created ConfigurationExplanation.dll and ConfigurationExplanation.dll reference files for the project.
    - Added JSON cache files for Razor and static web assets.
    - Introduced request.http file for testing API endpoints.
    - Included assembly manifest for application configuration.

--- ConfigurationExplanation/Program.cs
+++ ConfigurationExplanation/Program.cs
@@ -1,6 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// in memory configuration
+var inMemorySettings = new Dictionary<string, string>
+{
+    {"InMemoryKey", "InMemoryValue"}
+};
+builder.Configuration.AddInMemoryCollection(inMemorySettings);
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/{key}", (string key,IConfiguration configuration) =>
+{
+    return configuration[key] ?? "Key not found"; 
+} );
 
 app.Run();
