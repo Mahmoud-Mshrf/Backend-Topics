69ee79fb339cb384d92873c59a28e9c7aa339742
Mahmoud-Mshrf <104544236+Mahmoud-Mshrf@users.noreply.github.com>
2026-07-11 06:43:59 +0300

    Add generated NuGet files for DependencyInjectionExplanation project
    - Created DependencyInjectionExplanation.csproj.nuget.g.props to manage NuGet properties.
    - Added DependencyInjectionExplanation.csproj.nuget.g.targets for build targets.
    - Generated project.assets.json to define project dependencies and assets.
    - Included project.nuget.cache for caching NuGet package information.

---
+++ DependencyInjectionExplanation/Program.cs
@@ -0,0 +1,19 @@
+using DependencyInjectionExplanation.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+// how to register a service in the DI container
+
+// builder.Services.AddSingleton<IWeatherService, WeatherService>();
+// or using descriptor
+builder.Services.Add(new ServiceDescriptor(
+        typeof(IWeatherService),
+        typeof(WeatherService),
+        ServiceLifetime.Singleton));
+var app = builder.Build();
+
+app.MapGet("/{location}", async (string location, IWeatherService weatherService) =>
+{
+    var weatherInfo = await weatherService.GetWeatherInfoAsync(location);
+    return weatherInfo;
+});
+app.Run();
