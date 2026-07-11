8b4edcd51b1fd45b7d342f97c3dccadb96022a7b
Mahmoud-Mshrf <104544236+Mahmoud-Mshrf@users.noreply.github.com>
2026-07-11 23:09:42 +0300

    Add generated NuGet files for project assets and restore settings
    - Created MiddlewaresExplanation.csproj.nuget.g.props to define NuGet properties and restore settings.
    - Added MiddlewaresExplanation.csproj.nuget.g.targets for build targets.
    - Generated project.assets.json to manage project dependencies and assets.
    - Included project.nuget.cache to store cache information for NuGet operations.

--- DI/M02.DependencyInversionPrinciple/obj/Debug/net9.0/M02.DependencyInversionPrinciple.AssemblyInfo.cs
+++ DI/M02.DependencyInversionPrinciple/obj/Debug/net9.0/M02.DependencyInversionPrinciple.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("M02.DependencyInversionPrinciple")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf983e1f8af00ef10a32612d9b2e655febc7c5f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dafeeb79f2cb93500bf941d0d316096c82a78c51")]
 [assembly: System.Reflection.AssemblyProductAttribute("M02.DependencyInversionPrinciple")]
 [assembly: System.Reflection.AssemblyTitleAttribute("M02.DependencyInversionPrinciple")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
