c303242c172756c4bf61b8087e07e4e6dcff5f2b
Mahmoud-Mshrf <104544236+Mahmoud-Mshrf@users.noreply.github.com>
2026-07-12 01:24:11 +0300

    explain 2 ways to create middleware : app.use(Func<RequestDelegate,RequestDelegate> middleware) and app.use(Func<HttpContext,RequestDelegate,Task> middleware)

--- MiddlewaresExplanation/Program.cs
+++ MiddlewaresExplanation/Program.cs
@@ -1,6 +1,27 @@
+using System.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.Use((RequestDelegate next) =>
+{
+    return async (HttpContext context) =>
+    {
+        var sw = new Stopwatch();
+        sw.Start();
+        await next(context);
+        sw.Stop();
+        System.Console.WriteLine(
+            $"Request takes {sw.ElapsedMilliseconds} ms from middleware 1");
+    };
+});;
+
+app.Use(async(HttpContext context, RequestDelegate next) =>
+{
+   await context.Response.WriteAsJsonAsync("from middleware 2 ");
+   await next(context); 
+});
 app.Run();
+
