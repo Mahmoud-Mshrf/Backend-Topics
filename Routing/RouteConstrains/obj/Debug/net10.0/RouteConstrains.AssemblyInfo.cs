a5b7c3dc37d5b3c525dca0f567597c143e8e7922
Mahmoud-Mshrf <104544236+Mahmoud-Mshrf@users.noreply.github.com>
2026-07-24 02:17:52 +0300

    add project bare minimum authentication

--- Routing/RouteConstrains/obj/Debug/net10.0/RouteConstrains.AssemblyInfo.cs
+++ Routing/RouteConstrains/obj/Debug/net10.0/RouteConstrains.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RouteConstrains")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15fe06c35d1c862d90cd0a90617af9dddb8926e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06a5953a23566a891e0d203b6b663ad65a369b2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RouteConstrains")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RouteConstrains")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ SecurityInAspNetCore/BareMinimumAuthentication/Program.cs
@@ -0,0 +1,49 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)// register services required by authentication services
+.AddCookie();// add cookie authentication and it add CookieAuthenticationDefaults.AuthenticationScheme as Authentication scheme internally even you don't specify it
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.UseAuthentication();// enable authentication so its responsible for getting user info from cookies or headers ... and create the User in the httpContext 
+
+app.MapGet("/login",async context =>
+{
+   var claims = new List<Claim>
+   {
+       new ("Name","Mahmoud"),
+       new ("Sub",Guid.NewGuid().ToString()),
+       new ("Email","Mahmoud@localhost")
+   }; 
+
+   ClaimsIdentity identity = new ClaimsIdentity(claims,CookieAuthenticationDefaults.AuthenticationScheme);
+
+   var principal = new ClaimsPrincipal(identity);
+
+    await context.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,principal);
+});
+
+app.MapGet("/user", (HttpContext context) =>
+{
+    var principal = context.User;
+    if (principal.Identity is {IsAuthenticated:true})
+    {
+        var claims = principal.Claims.Select(x=>new {x.Type , x.Value});
+        return Results.Ok(claims);
+    }
+    return Results.Unauthorized();
+});
+
+app.MapGet("/logout",async (HttpContext context) =>
+{
+    await context.SignOutAsync();
+});
+
+app.Run();
+
+
+
